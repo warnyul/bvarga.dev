43d052c01d0adc81443e77eda9859181c758922b
Balázs Varga <warnyul@gmail.com>
2020-03-29 20:11:22 +0200

    Create deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,17 @@
 # Variables
 BASEDIR=$(dirname "$0")
 PROJECTDIR=$BASEDIR/..
+DOCKER_CONFIG_DIR=$PROJECTDIR/.docker
 
 set -a
 source ${PROJECTDIR}/.env
 set +a
 
-$BASEDIR/build.sh
+docker \
+  --tlsverify \
+  -H=$HOST:2376 \
+  --tlscacert=$DOCKER_CONFIG_DIR/ca.pem \
+  --tlscert=$DOCKER_CONFIG_DIR/cert.pem \
+  --tlskey=$DOCKER_CONFIG_DIR/key.pem \
+  stack deploy $DOCKER_STACK \
+  -c $PROJECTDIR/docker-compose.yml
