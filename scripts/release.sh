5af86dd4b2fdcc7983d96392f859a9d89cddef8c
Balázs Varga <warnyul@gmail.com>
2020-03-29 20:09:36 +0200

    Create release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,14 +3,19 @@
 # Variables
 BASEDIR=$(dirname "$0")
 PROJECTDIR=$BASEDIR/..
-VERSION=$(cat "${PROJECTDIR}/VERSION")
+VERSION=$(git describe --tags)
+IMAGE_NAME="warnyul/bvarga.dev"
 
 set -a
 source ${PROJECTDIR}/.env
 set +a
 
-docker build -t "warnyul/bvarga.dev" $PROJECTDIR
+$BASEDIR/build.sh
 
-docker login -u ${DOCKER_USER} -p {DOCKER_PASSWORD}
+docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
 
-docker tag "warnyul/bvarga.dev" "warnyul/bvarga.dev":""
+docker tag $IMAGE_NAME "$IMAGE_NAME:$VERSION"
+
+docker push $IMAGE_NAME
+
+docker logout
