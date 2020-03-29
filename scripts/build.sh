e7648674a1d851b4988646d17757ed41b0b5c89e
Balázs Varga <warnyul@gmail.com>
2020-03-29 20:05:58 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,5 @@
 BASEDIR=$(dirname "$0")
 PROJECTDIR=$BASEDIR/..
 IMAGE_NAME="warnyul/bvarga.dev"
-VERSION=$(date +%s)
 
-docker build -t "$IMAGE_NAME:$VERSION" $PROJECTDIR
+docker build -t "$IMAGE_NAME" $PROJECTDIR
