de7b0fa540d3e420f8a64cc071100b878a15dec9
Balázs Varga <warnyul@gmail.com>
2020-04-01 10:27:11 +0200

    Setup html and css minification

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM nginx:1.16.1-alpine
 
 COPY nginx.conf /etc/nginx/nginx.conf
-COPY src /usr/share/nginx/html
+COPY dist /usr/share/nginx/html
--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,32 @@
 BASEDIR=$(dirname "$0")
 PROJECTDIR=$BASEDIR/..
 IMAGE_NAME="warnyul/bvarga.dev"
+BUILDDIR=$PROJECTDIR/dist
+
+# Clean
+rm -rf $BUILDDIR
+mkdir -p $BUILDDIR
+
+# Install dependencies
+npm i
+
+# Minify css
+$PROJECTDIR/node_modules/css-minify/bin/css-minify.js css-minify -d  $PROJECTDIR/src/css
+mv $PROJECTDIR/css-dist $BUILDDIR/css
+
+$PROJECTDIR/node_modules/html-minifier/cli.js \
+    --collapse-whitespace \
+    --remove-comments \
+    --remove-optional-tags \
+    --remove-redundant-attributes \
+    --remove-tag-whitespace \
+    --use-short-doctype \
+    --minify-css true \
+    --minify-js true \
+    --input-dir $PROJECTDIR/src/html \
+    --output-dir $BUILDDIR
+
+# Copy fonts to dist folder
+cp -R $PROJECTDIR/src/assets/ $BUILDDIR
 
 docker build -t "$IMAGE_NAME" $PROJECTDIR
