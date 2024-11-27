c9f01b7f547928dc35f53572bd1f25d36a096bc6
Balázs Varga <balazs.varga@apter.tech>
2024-11-27 14:26:26 +0100

    x

--- webpack.config.js
+++ webpack.config.js
@@ -147,7 +147,7 @@ module.exports = {
       }
     }),
     new MangleCssClassPlugin({
-      classNameRegExp: '((fa|clazz)-([a-zA-Z0-9-]+)|fa(b?)|)',
+      classNameRegExp: '((fa|clazz)-([a-zA-Z0-9-]+)|fa(b?))',
       mangleCssVariables: true,
       log: true,
     }),
